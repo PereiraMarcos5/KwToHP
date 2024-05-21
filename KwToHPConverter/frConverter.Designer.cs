917f4111ae31af5ab99d1071c02c94047e8e5dc2
Marcos Vinícius Pereira <marcosvini.pp@gmail.com>
2024-05-21 10:57:06 -0300

    Initial commit

---
+++ KwToHPConverter/frConverter.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace KwToHPConverter
+{
+    partial class frConverter
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frConverter));
+            this.edKW = new DarkUI.Controls.DarkTextBox();
+            this.darkLabel1 = new DarkUI.Controls.DarkLabel();
+            this.edCV = new DarkUI.Controls.DarkTextBox();
+            this.darkLabel2 = new DarkUI.Controls.DarkLabel();
+            this.darkLabel3 = new DarkUI.Controls.DarkLabel();
+            this.darkLabel4 = new DarkUI.Controls.DarkLabel();
+            this.SuspendLayout();
+            // 
+            // edKW
+            // 
+            this.edKW.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
+            this.edKW.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.edKW.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.edKW.Location = new System.Drawing.Point(30, 47);
+            this.edKW.Name = "edKW";
+            this.edKW.Size = new System.Drawing.Size(160, 20);
+            this.edKW.TabIndex = 0;
+            this.edKW.TextChanged += new System.EventHandler(this.edKW_TextChanged);
+            // 
+            // darkLabel1
+            // 
+            this.darkLabel1.AutoSize = true;
+            this.darkLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel1.Location = new System.Drawing.Point(30, 28);
+            this.darkLabel1.Name = "darkLabel1";
+            this.darkLabel1.Size = new System.Drawing.Size(95, 13);
+            this.darkLabel1.TabIndex = 1;
+            this.darkLabel1.Text = "Potência em (kW):";
+            // 
+            // edCV
+            // 
+            this.edCV.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
+            this.edCV.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.edCV.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.edCV.Location = new System.Drawing.Point(33, 115);
+            this.edCV.Name = "edCV";
+            this.edCV.Size = new System.Drawing.Size(157, 20);
+            this.edCV.TabIndex = 2;
+            this.edCV.TextChanged += new System.EventHandler(this.edCV_TextChanged);
+            // 
+            // darkLabel2
+            // 
+            this.darkLabel2.AutoSize = true;
+            this.darkLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel2.Location = new System.Drawing.Point(30, 99);
+            this.darkLabel2.Name = "darkLabel2";
+            this.darkLabel2.Size = new System.Drawing.Size(92, 13);
+            this.darkLabel2.TabIndex = 3;
+            this.darkLabel2.Text = "Potência em (CV):";
+            // 
+            // darkLabel3
+            // 
+            this.darkLabel3.AutoSize = true;
+            this.darkLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel3.Location = new System.Drawing.Point(193, 51);
+            this.darkLabel3.Name = "darkLabel3";
+            this.darkLabel3.Size = new System.Drawing.Size(24, 13);
+            this.darkLabel3.TabIndex = 4;
+            this.darkLabel3.Text = "kW";
+            // 
+            // darkLabel4
+            // 
+            this.darkLabel4.AutoSize = true;
+            this.darkLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel4.Location = new System.Drawing.Point(194, 119);
+            this.darkLabel4.Name = "darkLabel4";
+            this.darkLabel4.Size = new System.Drawing.Size(22, 13);
+            this.darkLabel4.TabIndex = 5;
+            this.darkLabel4.Text = "HP";
+            // 
+            // frConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 168);
+            this.Controls.Add(this.darkLabel4);
+            this.Controls.Add(this.darkLabel3);
+            this.Controls.Add(this.darkLabel2);
+            this.Controls.Add(this.edCV);
+            this.Controls.Add(this.darkLabel1);
+            this.Controls.Add(this.edKW);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frConverter";
+            this.Text = "Converter HP/Kw";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DarkUI.Controls.DarkTextBox edKW;
+        private DarkUI.Controls.DarkLabel darkLabel1;
+        private DarkUI.Controls.DarkTextBox edCV;
+        private DarkUI.Controls.DarkLabel darkLabel2;
+        private DarkUI.Controls.DarkLabel darkLabel3;
+        private DarkUI.Controls.DarkLabel darkLabel4;
+    }
+}
+
---
+++ KwToHPConverter/frConverter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DarkUI;
+using DarkUI.Forms;
+
+namespace KwToHPConverter
+{
+    public partial class frConverter : DarkForm
+    {
+        private bool isUserInput; // Variável de controle
+
+        public frConverter()
+        {
+            InitializeComponent();
+            isUserInput = true;
+        }
+
+        private void edCV_TextChanged(object sender, EventArgs e)
+        {
+            if (isUserInput)
+            {
+                isUserInput = false;
+                try
+                {
+                    double kw = Double.Parse(edCV.Text) / 1.3596216173039;
+                    edKW.Text = kw.ToString();
+                }
+                catch (Exception E)
+                {
+                    edKW.Text = string.Empty;
+                }
+                isUserInput = true;
+            }
+        }
+
+        private void edKW_TextChanged(object sender, EventArgs e)
+        {
+            if (isUserInput)
+            {
+                isUserInput = false;
+                try
+                {
+                    double cavalosVapor = Double.Parse(edKW.Text) * 1.3596216173039;
+                    edCV.Text = cavalosVapor.ToString();
+                }
+                catch (Exception E)
+                {
+                    edCV.Text = string.Empty;
+                }
+                isUserInput = true;
+            }
+        }
+    }
+}
